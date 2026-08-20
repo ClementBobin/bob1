15592bfb5fb14f22c52d13be04fd3e4b863f7b3c
Clement Bobin <119869686+ClementBobin@users.noreply.github.com>
2026-08-20 14:58:53 +0200

    Update BASE_URL in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         buildConfigField(
             "String",
             "BASE_URL",
-            "\"${localProps.getProperty("BASE_URL", "https://bob-1-api-git-dev-clementbobins-projects.vercel.app")}\""
+            "\"${localProps.getProperty("BASE_URL", "https://zany-adventure-r9rjp499jx72x4w7-8080.app.github.dev")}\""
         )
     }
 
